d472db2edbebdbf9ecae79e040b6612f8f9d7057
Sarah Heinz. <124076207+SarahHeinz@users.noreply.github.com>
2024-07-14 09:19:40 +0200

    changed fotos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,82 @@
-const counter=0;
-const firstSelection = "";
-const secondSelection ="";
-const card = document.querySelector (".card");
+const cards = document.getElementsByClassName("card");
 
-// Click the card
 
-function show (){
-if (card)
-}
+const imagesLinkArray = [ 
+    { 
+        id: 1, 
+        image:  
+        'https://img.huffingtonpost.com/asset/579912bf2a00002d004f6e76.jpeg?ops=scalefit_720_noupscale&format=webp', 
+        newAlt: 'Angular Image'
+    }, 
+    { 
+        id: 2, 
+        image:  
+        'https://img.huffingtonpost.com/asset/579912d21200007404a53d62.jpeg?ops=scalefit_720_noupscale&format=webp', 
+        newAlt: 'HTML Image'
+    }, 
+    { 
+        id: 3, 
+        image:  
+        'https://img.huffingtonpost.com/asset/579914ca2a00002e004f6e7a.jpeg?ops=scalefit_720_noupscale&format=webp', 
+        newAlt: 'JavaScript Image'
+    }, 
+    { 
+        id: 4, 
+        image:  
+'https://img.huffingtonpost.com/asset/579914d92400002700b337c3.jpeg?ops=scalefit_720_noupscale&format=webp', 
+        newAlt: 'React Image'
+    }, 
+    { 
+        id: 5, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102833/vueImage.png', 
+        newAlt: 'Vue Image'
+    }, 
+    { 
+        id: 6, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102834/JSImage.jpg', 
+        newAlt: 'JavaScript Image'
+    }, 
+    { 
+        id: 7, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102833/vueImage.png', 
+        newAlt: 'Vue Image'
+    }, 
+    { 
+        id: 8, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102835/html5Image.png', 
+        newAlt: 'HTML Image'
+    }, 
+    { 
+        id: 9, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102834/CSS3Image.png', 
+        newAlt: 'CSS Image'
+    }, 
+    { 
+        id: 10, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102833/AngularImage.png', 
+        newAlt: 'Angular Image'
+    }, 
+    { 
+        id: 11, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102834/CSS3Image.png', 
+        newAlt: 'CSS Image'
+    }, 
+    { 
+        id: 12, 
+        image:  
+'https://media.geeksforgeeks.org/wp-content/uploads/20231122102833/reactImage.png', 
+        newAlt: 'React Image'
+    } 
+] 
+
+const allCards = document.getElementsByClassName ("card");
+ 
+
+if(thisImgSrc !== previousImgSrc)
